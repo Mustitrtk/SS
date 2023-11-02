7bcf9f97b9f15edf4fc84ee27d525f9a1feb0515
Mustafa Tortuk <mustafat237@hotmail.com>
2023-11-02 17:36:17 +0300

    Initial commit

---
+++ app/Actions/Fortify/CreateNewUser.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Actions\Fortify;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Laravel\Fortify\Contracts\CreatesNewUsers;
+use Laravel\Jetstream\Jetstream;
+
+class CreateNewUser implements CreatesNewUsers
+{
+    use PasswordValidationRules;
+
+    /**
+     * Validate and create a newly registered user.
+     *
+     * @param  array<string, string>  $input
+     */
+    public function create(array $input): User
+    {
+        Validator::make($input, [
+            'name' => ['required', 'string', 'max:255'],
+            'tc' => ['required', 'string', 'size:11', 'unique:users'],
+            'no' => ['required', 'string', 'size:11', 'unique:users'],
+            'password' => $this->passwordRules(),
+            'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature() ? ['accepted', 'required'] : '',
+        ])->validate();
+
+        return User::create([
+            'name' => $input['name'],
+            'tc' => $input['tc'],
+            'no' => $input['no'],
+            'password' => Hash::make($input['password']),
+        ])->assignRole('student');
+    }
+}
---
+++ resources/views/front/index.blade.php
@@ -0,0 +1,5 @@
+@extends('front.layout.app')
+
+@section('body')
+
+@endsection
